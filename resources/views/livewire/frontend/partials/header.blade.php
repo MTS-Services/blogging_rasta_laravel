aa94f72432c16e300b2a0480147d997b183b3204
Wasif Ahmed <155315416+wasifahmed85@users.noreply.github.com>
2025-11-14 16:07:20 +0600

    update

--- resources/views/livewire/frontend/partials/header.blade.php
+++ resources/views/livewire/frontend/partials/header.blade.php
@@ -17,7 +17,7 @@
                 {{ __('Home') }}
             </a>
             <a href="#" wire:navigate
-                class="text-text-muted font-inter transition-colors {{ request()->routeIs('products') ? 'text-second-500 border-b-2 border-second-500' : 'hover:text-second-500 hover:border-b-2 hover:border-second-500' }}">
+                class="text-text-muted font-inter transition-colors {{ request()->routeIs('products') ? 'text-second-500! border-b-2 border-second-500' : 'hover:text-second-500 hover:border-b-2 hover:border-second-500' }}">
                 {{ __('Products') }}
             </a>
             <a href="{{ route('video-feed') }}" wire:navigate
