68809941e222453b30d4721f8be4eb7e6736c154
Wasif Ahmed <155315416+wasifahmed85@users.noreply.github.com>
2025-11-16 12:28:30 +0600

    model migraton done

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -2,9 +2,28 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use App\Enums\BlogStatus;
+use OwenIt\Auditing\Contracts\Auditable;
 
-class Blog extends Model
+use App\Models\BaseModel;
+
+class Blog extends BaseModel implements Auditable
 {
-    //
+       protected $fillable = [
+        'sort_order',
+        'title',
+        'slug',
+        'status',
+        'file',
+        'description',
+        'meta_title',
+        'meta_description',
+        'meta_keywords',
+    ];
+
+    protected $casts = [
+        'status' => BlogStatus::class,
+         'meta_keywords' => 'array',
+    ];
+
 }
--- database/migrations/2025_11_16_062331_create_blogs_table.php
+++ database/migrations/2025_11_16_062331_create_blogs_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Enums\BlogStatus;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +14,16 @@ return new class extends Migration
     {
         Schema::create('blogs', function (Blueprint $table) {
             $table->id();
+            $table->bigInteger('sort_order')->default(0);
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->string('status')->index()->default(BlogStatus::PUBLISHED->value);
+
+            $table->string('file');
+            $table->longText('description')->nullable();
+            $table->string('meta_title')->nullable();
+            $table->longText('meta_description')->nullable();
+            $table->json('meta_keywords')->nullable();
             $table->timestamps();
         });
     }
