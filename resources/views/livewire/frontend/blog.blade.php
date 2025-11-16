f354c6181ef5b25e825b0e5135de8836baf5ca83
Wasif Ahmed <155315416+wasifahmed85@users.noreply.github.com>
2025-11-16 18:12:14 +0600

    Update blog.blade.php

--- resources/views/livewire/frontend/blog.blade.php
+++ resources/views/livewire/frontend/blog.blade.php
@@ -14,7 +14,7 @@
                     </div>
 
                     <div class="lg:w-1/2 mt-8 lg:mt-0">
-                        <div class="w-auto sm:w-[400px] h-auto sm:h-[600px] mx-auto">
+                        <div class="w-auto sm:w-[450px] h-auto sm:h-[600px] mx-auto">
                            <x-blog-media :file="$blog->file" />
                         </div>
                     </div>
@@ -26,14 +26,14 @@
             @if($layout === 1)
                 <div class="block lg:flex gap-12 items-center justify-between mt-20">
                     <div class="lg:w-1/2">
-                        <div class="w-auto sm:w-[600px] h-auto sm:h-[600px] mx-auto">
+                        <div class="w-auto sm:w-[450px] h-auto sm:h-[600px] mx-auto">
                            <x-blog-media :file="$blog->file" />
                         </div>
                     </div>
 
                     <div class="bg-second-500/15 p-6 lg:w-1/2 mt-8 lg:mt-0">
                         <h3 class="text-3xl font-semibold text-text-primary">{{ $blog->title }}</h3>
-                        <p class="text-base mt-4 text-text-primary">{{ $blog->description }}</p>
+                        <p class="text-base mt-4 text-text-primary">{!! $blog->description !!}</p>
                     </div>
                 </div>
             @endif
@@ -44,11 +44,11 @@
                 <div class="block lg:flex gap-12 items-center justify-between mt-20">
                     <div class="bg-blog p-6 lg:w-1/2">
                         <h3 class="text-3xl font-semibold text-text-primary">{{ $blog->title }}</h3>
-                        <p class="text-base mt-4 text-text-primary">{{ $blog->description }}</p>
+                        <p class="text-base mt-4 text-text-primary">{!! $blog->description !!}</p>
                     </div>
 
                     <div class="lg:w-1/2 mt-8 lg:mt-0">
-                        <div class="w-auto sm:w-[600px] h-auto sm:h-[1080px] mx-auto">
+                        <div class="w-auto sm:w-[450px] h-auto sm:h-[600px] mx-auto">
                            <x-blog-media :file="$blog->file" />
                         </div>
                     </div>
