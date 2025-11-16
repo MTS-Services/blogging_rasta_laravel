8b859b87f2583841a99419696c0188ddd732b2e0
Wasif Ahmed <155315416+wasifahmed85@users.noreply.github.com>
2025-11-16 11:48:30 +0600

    Update header.blade.php

--- resources/views/livewire/frontend/partials/header.blade.php
+++ resources/views/livewire/frontend/partials/header.blade.php
@@ -16,10 +16,10 @@
                 class="text-text-muted font-inter transition-colors {{ request()->routeIs('home') ? 'text-second-500! border-b-2 border-second-500' : 'hover:text-second-500! hover:border-b-2 hover:border-second-500' }}">
                 {{ __('Home') }}
             </a>
-            <a href="{{ route('tiktok-mixed-feed') }}" wire:navigate
+            {{-- <a href="{{ route('tiktok-mixed-feed') }}" wire:navigate
                 class="text-text-muted font-inter transition-colors {{ request()->routeIs('tiktok-mixed-feed') ? 'text-second-500! border-b-2 border-second-500' : 'hover:text-second-500 hover:border-b-2 hover:border-second-500' }}">
                 {{ __('Tiktok') }}
-            </a>
+            </a> --}}
             <a href="{{ route('product') }}" wire:navigate
                 class="text-text-muted font-inter transition-colors {{ request()->routeIs('product') ? 'text-second-500! border-b-2 border-second-500' : 'hover:text-second-500 hover:border-b-2 hover:border-second-500' }}">
                 {{ __('Products') }}
