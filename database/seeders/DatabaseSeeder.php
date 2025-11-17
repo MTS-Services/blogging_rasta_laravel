5998559d1eecb115d65815e08216b36d010d1179
dev-sumon <akhtaruzzamansumon7@gmail.com>
2025-11-17 14:31:33 +0600

    category seeder added

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,6 +20,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             ApplicationSettingSeeder::class,
             BlogSeeder::class,
+            CategorySeeder::class,
         ]);
     }
 }
