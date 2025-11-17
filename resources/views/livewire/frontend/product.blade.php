9b75cd5f3e7b01d08dc755284e9af20f25581895
dev-sumon <akhtaruzzamansumon7@gmail.com>
2025-11-17 18:37:37 +0600

    product dynamic in progress

--- resources/views/livewire/frontend/product.blade.php
+++ resources/views/livewire/frontend/product.blade.php
@@ -11,33 +11,64 @@
                 <div class="group w-full p-6 border border-zinc-300/40 rounded-2xl">
                     {{-- Thumbnail --}}
                     <div class="relative w-full h-[300px] overflow-hidden rounded-2xl">
-                        <img src="{{ asset('assets/images/product/' . $product['thumb']) }}" alt="TikTok thumbnail"
+                        <img src="{{ storage_url($product->image) }}" alt="TikTok thumbnail"
                             class="w-full h-full object-cover transition-transform duration-300 group-hover:scale-105">
 
                     </div>
                     <div class="mt-2">
-                        <p class="text-xs text-text-primary font-normal font-outfit">{{ __('Essences') }}</p>
-                        <h4 class="text-xl font-lato font-medium text-text-primary">{{ ($product['name']) }}
+                        <p class="text-xs text-text-primary font-normal font-outfit">{{ $product->category->title }}
+                        </p>
+                        <h4 class="text-xl font-lato font-medium text-text-primary">{{ $product->title }}
                         </h4>
                         <div class="flex items-center justify-between">
                             <div class="mt-2">
-                                <h3 class="text-2xl font-lato font-playfair text-primary-950/60">{{ __('$12') }}
-                                </h3>
+                                {{-- <h3 class="text-2xl font-playfair text-primary-950/60">{{ '$' . $product->price }}
+                                </h3> --}}
+                                <div>
+                                    @if ($product->sale_price)
+                                        {{-- যদি sale price থাকে --}}
+                                        <h3 class="text-2xl font-playfair text-primary-950/60 line-through">
+                                            {{ '$' . $product->price }}
+                                        </h3>
+                                        <h3 class="text-2xl font-playfair text-zinc-500 font-bold">
+                                            {{ '$' . $product->sale_price }}
+                                        </h3>
+                                    @else
+                                        {{-- যদি sale price না থাকে --}}
+                                        <h3 class="text-2xl font-playfair text-primary-950/60">
+                                            {{ '$' . $product->price }}
+                                        </h3>
+                                    @endif
+                                </div>
                             </div>
-                            <div class="flex items-center">
+                            {{-- <div class="flex items-center">
                                 <flux:icon name="star" class="w-3 h-3 stroke-second-500" />
                                 <flux:icon name="star" class="w-3 h-3 stroke-second-500" />
                                 <flux:icon name="star" class="w-3 h-3 stroke-second-500" />
                                 <flux:icon name="star" class="w-3 h-3 stroke-second-500" />
                                 <flux:icon name="star" class="w-3 h-3 stroke-muted" />
                                 <p>{{ __('(324)') }}</p>
-                            </div>
+                            </div> --}}
                         </div>
                         <div class="flex items-center gap-2 mt-2">
-                            <p class="text-base font-normal font-inter text-second-500 py-1 px-2.5 bg-second-500/10">
-                                {{ __('Dry') }}</p>
-                            <p class="text-base font-normal font-inter text-second-500 py-1 px-2.5 bg-second-500/10">
-                                {{ __('Sensetive') }}</p>
+
+                            @php
+                                $types = json_decode($product->product_types, true);
+                            @endphp
+
+                            @if ($types && is_array($types))
+                                @foreach ($types as $type)
+                                    <p
+                                        class="text-base font-normal font-inter text-second-500 py-1 px-2.5 bg-second-500/10">
+                                        {{ $type }}
+                                    </p>
+                                @endforeach
+                            @else
+                                <p
+                                    class="text-base font-normal font-inter text-second-500 py-1 px-2.5 bg-second-500/10">
+                                    {{ __('N/A') }}
+                                </p>
+                            @endif
                         </div>
                         <div class="w-full! mt-2">
                             <x-ui.button href="#"
