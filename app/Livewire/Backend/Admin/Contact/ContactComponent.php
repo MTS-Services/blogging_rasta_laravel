9fe4666b78cf460b77ffc5d97a41732571e64e70
Wasif Ahmed <155315416+wasifahmed85@users.noreply.github.com>
2025-11-18 12:55:48 +0600

    update

---
+++ app/Livewire/Backend/Admin/Contact/ContactComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Backend\Admin\Contact;
+
+use Livewire\Component;
+
+class ContactComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.backend.admin.contact.contact-component');
+    }
+}
