dbec0903e7265c062ec57e1da55aa8ec532fe0c7
Wasif Ahmed <155315416+wasifahmed85@users.noreply.github.com>
2025-11-18 08:37:15 +0600

    Update blog.blade.php

--- resources/views/livewire/frontend/blog.blade.php
+++ resources/views/livewire/frontend/blog.blade.php
@@ -8,14 +8,18 @@
             @if ($layout === 0)
                 <div class="flex gap-12 items-center justify-between mt-12">
                     <div class="bg-second-500/15 p-6 flex-1 lg:flex-1/2">
-                        <h3 class="text-3xl font-semibold text-text-primary">{{ Str::limit($blog->title, 18, '..') }}
+
+                        <h3 class="text-3xl font-semibold text-text-primary line-clamp-2">
+                            {{ $blog->title }}
                         </h3>
+
                         <a href="{{ route('blog.details', $blog->slug) }}" wire:navigate
-                            class="inline-block mt-4 text-text-secondary">
-                            <div>
-                                {!! Str::limit(html_entity_decode($blog->description), 800, '...') !!}
+                           class="inline-block mt-4 text-text-secondary">
+                            <div class="line-clamp-6">
+                                {!! $blog->description !!}
                             </div>
                         </a>
+
                     </div>
                     <div class="flex-1 lg:flex-1/2 mt-8 lg:mt-0 flex justify-center">
                         <x-blog-media :file="$blog->file" class="!object-cover" />
@@ -26,17 +30,22 @@
             @if ($layout === 1)
                 <div class="flex gap-12 items-center justify-between mt-12 lg:mt-20">
                     <div class="flex-1 lg:flex-1/2 flex justify-center">
-                        <x-blog-media :file="$blog->file" class="!object-cover " />
+                        <x-blog-media :file="$blog->file" class="!object-cover" />
                     </div>
+
                     <div class="bg-blog p-6 flex-1 lg:flex-1/2 mt-8 lg:mt-0">
-                        <h3 class="text-3xl font-semibold text-text-primary">{{ Str::limit($blog->title, 18, '..') }}
+
+                        <h3 class="text-3xl font-semibold text-text-primary line-clamp-2">
+                            {{ $blog->title }}
                         </h3>
+
                         <a href="{{ route('blog.details', $blog->slug) }}" wire:navigate
-                            class="inline-block mt-4 text-text-secondary">
-                            <div>
-                                {!! Str::limit(html_entity_decode($blog->description), 800, '...') !!}
+                           class="inline-block mt-4 text-text-secondary">
+                            <div class="line-clamp-6">
+                                {!! $blog->description !!}
                             </div>
                         </a>
+
                     </div>
                 </div>
             @endif
